3eedcfd3bcebef8ab7b89d48eaa0f77577d31edf
你的新用户名 <12>
2025-02-19 16:20:46 +0800

    重构优化

--- src/reactivity/reactive.ts
+++ src/reactivity/reactive.ts
@@ -3,11 +3,14 @@ import { mutableHandler,readOnlyHandler } from "./baseHandlers"
 
 
 export function reactive(raw:Object){
-    return new Proxy(raw,mutableHandler)
+    return createActiveObject(raw,mutableHandler)
 }
 
 
 export function readOnly(raw:Object){
-    return new Proxy(raw,readOnlyHandler)
+    return createActiveObject(raw,readOnlyHandler)
 }
 
+function createActiveObject(raw:any,handler){
+    return new Proxy(raw,handler)
+}

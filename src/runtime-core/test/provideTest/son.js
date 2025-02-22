4f443a0aa817ffd6915624cb66b28fdf753e62d8
你的新用户名 <12>
2025-02-22 22:23:31 +0800

    实现inject默认值可以为函数

--- src/runtime-core/test/provideTest/son.js
+++ src/runtime-core/test/provideTest/son.js
@@ -3,17 +3,18 @@ import soo from './grandSon.js'
 export default {
     render(){
         
-        return h('div',{},[createTextNode(this.father),h(soo,{}),createTextNode(this.m)])},
+        return h('div',{},[createTextNode(this.father),h(soo,{}),createTextNode(this.m),createTextNode(this.n)])},
     // 通过setup函数可以接收props
     setup(props){
         const father = inject('father')
         const m = inject('m','mommy')
-
+        const n = inject('n',()=>'function')
         provide('father','father2')
         console.log(father);
         return{
             father,
-            m
+            m,
+            n
         }
     }
 }
